05bfc88552ab35cab798f5c14b85af2e49845178
janati <janati@lbgi.fr>
2024-11-29 14:15:03 +0100

    fichier api modifié et app commenté

--- api.py
+++ api.py
@@ -1,77 +1,241 @@
-from flask import Flask, Blueprint, abort, jsonify, render_template
+from flask import Flask, Blueprint, abort, jsonify, render_template, request
 import os
 import sys
 import yaml
+from src.geneontology import GeneOntology
+from src.taxonomy import Taxonomy
+from src.interpro import Interpro
+
 
 curdir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(f'{curdir}/src')
+sys.path.append(f"{curdir}/src")
 
 from src.warehouse import Warehouse
 
-with open('config.yml', 'r') as config_file:
+with open("config.yml", "r") as config_file:
     config = yaml.safe_load(config_file)
-wh = Warehouse(config['warehouse'])
+wh = Warehouse(config["warehouse"])
+go = GeneOntology(config["geneontology"])
+taxo = Taxonomy()
+interpro = Interpro()
 
-bp = Blueprint('api', __name__)
+bp = Blueprint("api", __name__)
+
 
 def api_response(data, nb=-1):
-    res = {'meta': {'status': 'success'}}
-    res['meta']['nbResults'] = nb if nb >= 0 else len(data)
-    res['data'] = data
+    res = {"meta": {"status": "success"}}
+    res["meta"]["nbResults"] = nb if nb >= 0 else len(data)
+    res["data"] = data
     return res
+
 
 @bp.errorhandler(404)
 def page_not_found(e):
-    return jsonify({'meta': {'status': 'error', 'msg': e.description}}), 404
+    return jsonify({"meta": {"status": "error", "msg": e.description}}), 404
 
+
+# No function to get the list of databases --> fixed
+# Does not work on localhost
 @bp.route("/<database>")
 def api_index(database):
     db = wh.get_db(database)
     if not db:
         abort(404, f'Unknown database "{database}".')
-    return render_template('apiui.html', db=database, prefix=config['prefix'])
+    return render_template("apiui.html", db=database, prefix=config["prefix"])
 
+
+# Does not work on localhost
 @bp.route("/desc/<database>.yml")
 def api_desc(database):
     db = wh.get_db(database)
     if not db:
         abort(404, f'Unknown database "{database}".')
-    return render_template('apidesc.yml', db=database, prefix=config['prefix'])
+    return render_template("apidesc.yml", db=database, prefix=config["prefix"])
 
-@bp.route("/<database>/species")
-def list_species(database):
-    db = wh.get_db(database)
+
+# works correctly
+@bp.route("/<database>/<int:release>/species")
+def list_species(database, release):
+    db = wh.get_db(database, release)
     if not db:
         abort(404, f'Unknown database "{database}".')
     species = db.get_species_list()
     return jsonify(api_response(species))
 
-@bp.route("/<database>/species/<taxid>/proteins")
-def list_proteins(database, taxid):
-    db = wh.get_db(database)
+
+# works
+@bp.route("/<database>/<int:release>/species/<taxid>/proteins")
+def list_proteins(database, release, taxid):
+    db = wh.get_db(database, release)
     if not db:
         abort(404, f'Unknown database "{database}".')
     res = db.get_species_proteins(taxid)
     return jsonify(api_response(res))
 
-@bp.route("/<database>/protein/<access>")
-def desc_protein(database, access):
-    db = wh.get_db(database)
+
+# works
+@bp.route("/<database>/<int:release>/protein/<access>")
+def desc_protein(database, release, access):
+    db = wh.get_db(database, release)
     if not db:
         abort(404, f'Unknown database "{database}".')
     return jsonify(api_response(db.get_protein_api(access), nb=1))
 
-@bp.route("/<database>/protein/<access>/orthologs")
-def orthologs(database, access):
-    db = wh.get_db(database)
+
+# works
+@bp.route("/<database>/<int:release>/protein/<access>/orthologs")
+def orthologs(database, release, access):
+    db = wh.get_db(database, release)
     if not db:
         abort(404, f'Unknown database "{database}".')
     return jsonify(api_response(db.get_orthologs_api(access)))
 
-@bp.route("/<database>/protein/<access>/orthologs/{taxid}")
+
+# Doesn't work
+@bp.route("/<database>/<int:release>/protein/<access>/orthologs/{taxid}")
 def orthologs_species(database, access, taxid):
     pass
 
-@bp.route("/<database>/species/<taxid1>/orthologs/<taxid2>")
-def orthologs_two_species(database, taxid1, taxid2):
-    pass
+
+# Doesn't work
+#@bp.route("/<database>/<int:release>/species/<taxid1>/orthologs/<taxid2>")
+#def orthologs_two_species(database, taxid1, taxid2):
+#    pass
+
+
+##############################################"
+
+# Added API:
+
+
+# get the clades stored in the config.yml file
+@bp.route("/<database>/<int:release>/clades/rough")
+def get_clades_rough(database, release):
+    db = wh.get_db(database, release)
+    return jsonify(db.clades)
+
+
+# get all clades present in the sun tree, more detailed
+@bp.route("/<database>/<int:release>/clades/detailed")
+def get_clades_detailed(database, release):
+    db = wh.get_db(database, release)
+    tree = db.get_sun_tree()
+    clade_list = []
+    for clade in tree:
+        # Use a dictionary to store clade information
+        infos = {"id": clade["id"], "name": clade["name"]}
+        clade_list.append(infos)
+    return jsonify(clade_list)
+
+
+# profiles of all the proteins in a species
+@bp.route("/<database>/<int:release>/species/<taxid>/profiles")
+def get_all_profiles(database, release, taxid):
+    db = wh.get_db(database, release)
+    proteins = db.get_species_proteins(taxid)
+    profiles = {}
+    for entry in proteins:
+        # build the list of profiles
+        prot = db.get_protein(entry["access"])
+        # dist = db.build_distribution(prot, model_only=1) Could be used to get all the distributions
+        profiles[prot["access"]] = prot[
+            "profile"
+        ]  # for later: include the model_only argument as used by the profiles function
+    return jsonify(profiles) # each bit is organized in the order of the species in the database from whence they came
+
+
+# get list of databases
+@bp.route("/databases")
+def databases_list():
+    databases = {}
+    years = config["warehouse"]["databases"].keys()
+    for year in years:
+        databases[year] = list(config["warehouse"]["databases"][year].keys())
+    return jsonify(databases)
+
+
+# list of species in clade
+@bp.route("/<database>/<int:release>/<clade>/species")
+def get_clade_species(database, release, clade):
+    db = wh.get_db(database, release)
+    species_list = []
+    sql = db._get_sql("get_clade_species")
+    result = db._query(
+        sql, {"clade": clade}
+    )  # uses the text contained in the get_clade_species.sql file as template for the query
+    for species in result:
+        species_list.append({"taxid": species["taxid"], "name": species["name"]})
+    return jsonify(species_list)
+
+# orthologs btw 2 species
+@bp.route("/<database>/<int:release>/species/<taxid1>/orthologs/<taxid2>")
+def orthologs_two_species(database, release, taxid1, taxid2):
+    db = wh.get_db(database, release)
+    ortholog_list = []
+    sql = db._get_sql("orthologie2finale")
+    orthologs = db._query(
+        sql, {"species1": taxid1, "species2": taxid2}
+    )
+    return jsonify(orthologs)
+
+#######################################
+
+
+# works http://localhost:5000/oi/api/Eukaryota/2023/protein/Q6KAQ7
+@bp.route("/<dbname>/<int:release>/proximal/<access>")
+def proximal(dbname, release, access):
+    db = wh.get_db(dbname, release)
+    if not db or not db.has_distances:
+        abort(404)
+    return jsonify(db.get_proximal_proteins(access))
+
+
+# works after importing request http://localhost:5000/oi/api/Eukaryota/2023/distribution/Q6KAQ7
+# Get a list of species instead of clade
+@bp.route("/<dbname>/<int:release>/distribution/<access>")
+@bp.route("/<dbname>/<int:release>/distribution/<access>/full")
+def distribution(dbname, release, access):
+    db = wh.get_db(dbname, release)
+    if not db or not db.has_distances:
+        abort(404)
+    if not db.has_profiles or not db.has_clades:
+        abort(404)
+    model_only = not request.base_url.endswith("full")
+    prot = db.get_protein(access)
+    dist = db.build_distribution(prot, model_only=model_only)
+    return jsonify(dist)
+
+
+# works http://localhost:5000/oi/api/Archaea/2023/tree/profile
+@bp.route("/<database>/<int:release>/tree/profile")
+def species_tree_profile(database, release):
+    db = wh.get_db(database, release)
+    if not db:
+        abort(404)
+    tree = db.get_profile_tree()
+    return jsonify(tree)
+
+
+# works http://localhost:5000/oi/api/annotations/go/Q6KAQ7
+@bp.route("/annotations/go/<access>")
+def go_annotations(access):
+    return jsonify(go.get_annotations(access))
+
+
+# works http://localhost:5000/oi/api/annotations/interpro/C4LID5
+@bp.route("/annotations/interpro/<access>")
+def interpro_annotations(access):
+    annots = interpro.get_domains(access)
+    if not annots:
+        abort(404)
+    return jsonify(annots)
+
+
+# works http://localhost:5000/oi/api/Eukaryota/2023/search/protein?term=MTH
+@bp.route("/<database>/<int:release>/search/protein")
+def search_protein(database, release):
+    db = wh.get_db(database, release)
+    if not db:
+        abort(404)
+    pattern = request.args.get("term")
+    return jsonify(db.search_protein(pattern))
--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 import os
 import sys
-from flask import Flask, request, render_template, Blueprint, abort, redirect, jsonify, Response, url_for
+from flask import (
+    Flask,
+    request,
+    render_template,
+    Blueprint,
+    abort,
+    redirect,
+    jsonify,
+    Response,
+    url_for,
+)
 import jinja2
 import yaml
 import json
 import re
 
 curdir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(f'{curdir}/src')
+sys.path.append(f"{curdir}/src")
 
 from src.warehouse import Warehouse
 from src.geneontology import GeneOntology
@@ -18,33 +28,42 @@ from api import bp as bpapi
 from secret import bp as bpsecret
 
 
-with open('config.yml', 'r') as config_file:
+with open("config.yml", "r") as config_file:
     config = yaml.safe_load(config_file)
-wh = Warehouse(config['warehouse'])
-go = GeneOntology(config['geneontology'])
+wh = Warehouse(config["warehouse"])
+go = GeneOntology(config["geneontology"])
 taxo = Taxonomy()
 interpro = Interpro()
 
 app = Flask(__name__, static_url_path=f"/{config['prefix']}/static")
-bp = Blueprint('bp', __name__)
+bp = Blueprint("bp", __name__)
+
 
 def regex_match(string, pattern):
     return re.match(pattern, string) is not None
 
-app.jinja_env.filters['regex_match'] = regex_match
+
+app.jinja_env.filters["regex_match"] = regex_match
+
 
 @bp.context_processor
 def inject_globals():
     return config
 
+
 @bp.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html', dblist=wh.get_dblist()), 404
+    return render_template("404.html", dblist=wh.get_dblist()), 404
+
 
 @bp.route("/")
 def home():
-    return render_template('index.html', dblist=wh.get_dblist(), stats=wh.gather_stats())
+    return render_template(
+        "index.html", dblist=wh.get_dblist(), stats=wh.gather_stats()
+    )
 
+
+# does not work http://localhost:5000/oi/Archaea/2023
 @bp.route("/<database>/<int:release>")
 def db_home(database, release):
     db = wh.get_db(database, release)
@@ -53,8 +72,17 @@ def db_home(database, release):
     status = db.get_status()
     stats = db.get_stats()
     species = db.get_species_list()
-    return render_template('dbindex.html', dblist=wh.get_dblist(), db=db.get_info(), status=status, stats=stats, species=species)
+    return render_template(
+        "dbindex.html",
+        dblist=wh.get_dblist(),
+        db=db.get_info(),
+        status=status,
+        stats=stats,
+        species=species,
+    )
 
+
+# works http://localhost:5000/oi/Archaea/2023/species
 @bp.route("/<database>/<int:release>/species")
 def species_list(database, release):
     db = wh.get_db(database, release)
@@ -62,6 +90,9 @@ def species_list(database, release):
         abort(404)
     return jsonify(db.get_simple_species_list())
 
+
+# works http://localhost:5000/oi/Archaea/2023/tree/sun
+@bp.route("/api/<database>/<int:release>/tree/sun")
 @bp.route("/<database>/<int:release>/tree/sun")
 def species_tree_sun(database, release):
     db = wh.get_db(database, release)
@@ -70,6 +101,8 @@ def species_tree_sun(database, release):
     tree = db.get_sun_tree(maxdepth=12)
     return jsonify(tree)
 
+
+# works http://localhost:5000/oi/Archaea/2023/tree/profile
 @bp.route("/<database>/<int:release>/tree/profile")
 def species_tree_profile(database, release):
     db = wh.get_db(database, release)
@@ -78,6 +111,8 @@ def species_tree_profile(database, release):
     tree = db.get_profile_tree()
     return jsonify(tree)
 
+
+# works http://localhost:5000/oi/Eukaryota/2023/search/protein?term=MTH
 @bp.route("/<database>/<int:release>/search/protein")
 def search_protein(database, release):
     db = wh.get_db(database, release)
@@ -86,31 +121,47 @@ def search_protein(database, release):
     pattern = request.args.get("term")
     return jsonify(db.search_protein(pattern))
 
+
+# Works
 @bp.route("/<database>/<int:release>/protein/<access>")
 @bp.route("/<database>/<int:release>/protein/<access>/full")
 def protein(database, release, access):
     db = wh.get_db(database, release)
     if not db:
         abort(404)
-    model_only = not request.base_url.endswith('full')
+    model_only = not request.base_url.endswith("full")
     if not db.has_models and model_only:
-        return redirect(url_for('bp.protein', database=database, release=release, access=access), code=302)
+        return redirect(
+            url_for("bp.protein", database=database, release=release, access=access),
+            code=302,
+        )
     prot = db.get_protein(access)
     if not prot:
         abort(404)
     primarydb = wh.get_primary_database(release, prot)
-    return render_template('protein.html', dblist=wh.get_dblist(), db=db.get_info(), protein=prot, model=model_only, primarydb=primarydb)
+    return render_template(
+        "protein.html",
+        dblist=wh.get_dblist(),
+        db=db.get_info(),
+        protein=prot,
+        model=model_only,
+        primarydb=primarydb,
+    )
 
+
+# Doesn't work with dbname but with database yes: http://localhost:5000/oi/Eukaryota/2016/orthologs/P83120, not OrthoInspector_Eukaryota_2023 (as contained in config.yml)
 @bp.route("/<dbname>/<int:release>/orthologs/<access>")
 @bp.route("/<dbname>/<int:release>/orthologs/<access>/full")
 def orthologs(dbname, release, access):
     db = wh.get_db(dbname, release)
     if not db:
         abort(404)
-    model_only = not request.base_url.endswith('full')
+    model_only = not request.base_url.endswith("full")
     orthos = db.get_orthologs(access, model=model_only)
     return jsonify(orthos)
 
+
+# works http://localhost:5000/oi/Eukaryota/2023/protein/Q6KAQ7
 @bp.route("/<dbname>/<int:release>/proximal/<access>")
 def proximal(dbname, release, access):
     db = wh.get_db(dbname, release)
@@ -118,6 +169,8 @@ def proximal(dbname, release, access):
         abort(404)
     return jsonify(db.get_proximal_proteins(access))
 
+
+# works http://localhost:5000/oi/Eukaryota/2023/distribution/Q6KAQ7
 @bp.route("/<dbname>/<int:release>/distribution/<access>")
 @bp.route("/<dbname>/<int:release>/distribution/<access>/full")
 def distribution(dbname, release, access):
@@ -126,28 +179,34 @@ def distribution(dbname, release, access):
         abort(404)
     if not db.has_profiles or not db.has_clades:
         abort(404)
-    model_only = not request.base_url.endswith('full')
+    model_only = not request.base_url.endswith("full")
     prot = db.get_protein(access)
     dist = db.build_distribution(prot, model_only=model_only)
     return jsonify(dist)
 
-@bp.route("/<database>/<int:release>/download/fasta", methods=['POST'])
+
+# should work
+@bp.route("/<database>/<int:release>/download/fasta", methods=["POST"])
 def download_fasta(database, release):
     db = wh.get_db(database, release)
     if not db:
         abort(404)
-    access_list = request.form['access_list'].split(',')
+    access_list = request.form["access_list"].split(",")
     fasta = db.get_fasta(access_list)
-    res = Response(fasta, mimetype='text/plain')
-    filename = request.form.get('filename', None)
+    res = Response(fasta, mimetype="text/plain")
+    filename = request.form.get("filename", None)
     if filename:
-        res.headers['Content-Disposition'] = f'attachment; filename="{filename}"'
+        res.headers["Content-Disposition"] = f'attachment; filename="{filename}"'
     return res
 
+
+# works http://localhost:5000/oi/annotations/go/Q6KAQ7
 @bp.route("/annotations/go/<access>")
 def go_annotations(access):
     return jsonify(go.get_annotations(access))
 
+
+# works
 @bp.route("/annotations/interpro/<access>")
 def interpro_annotations(access):
     annots = interpro.get_domains(access)
@@ -155,56 +214,78 @@ def interpro_annotations(access):
         abort(404)
     return jsonify(annots)
 
+
+# Not implemented
 @bp.route("/stats")
 def do_stats():
-    return render_template('dbstats.html', stats=wh.get_stats())
+    return render_template("dbstats.html", stats=wh.get_stats())
 
-@bp.route("/blast", methods=['GET', 'POST'])
+
+@bp.route("/blast", methods=["GET", "POST"])
 def blast_search():
-    query = request.form['query'] if request.method == 'POST' else ''
-    return render_template('blastsearch.html', dblist=wh.get_dblist(), query=query)
+    query = request.form["query"] if request.method == "POST" else ""
+    return render_template("blastsearch.html", dblist=wh.get_dblist(), query=query)
 
-@bp.route("/blast/submit", methods=['POST'])
+
+@bp.route("/blast/submit", methods=["POST"])
 def blast_search_run():
-    res = submit_task(config['worker_pool']['host'], 'blast_search', request.form.to_dict())
+    res = submit_task(
+        config["worker_pool"]["host"], "blast_search", request.form.to_dict()
+    )
     return jsonify(res)
+
 
 @bp.route("/blast/result/<taskid>")
 def blast_search_res(taskid):
-    res = check_task(config['worker_pool']['host'], taskid)
+    res = check_task(config["worker_pool"]["host"], taskid)
     return jsonify(res)
+
 
 @bp.route("/profilesearch")
 def profile_search():
-    return render_template('profilesearch.html', dblist=wh.get_dblist())
+    return render_template("profilesearch.html", dblist=wh.get_dblist())
 
-@bp.route("/profilesearch/result", methods=['POST'])
+
+@bp.route("/profilesearch/result", methods=["POST"])
 def profile_search_run():
     params = {}
-    for key in ['database', 'release', 'query', 'present', 'absent', 'display']:
+    for key in ["database", "release", "query", "present", "absent", "display"]:
         params[key] = request.form[key]
     params = {k: json.loads(v) for k, v in params.items()}
     job_params = {k: v for k, v in params.items() if k != "display"}
-    res = submit_task(config['worker_pool']['host'], 'profile_search', job_params)
-    return render_template('profilesearchresult.html', params=params, taskid=res['id'], dblist=wh.get_dblist())
+    res = submit_task(config["worker_pool"]["host"], "profile_search", job_params)
+    return render_template(
+        "profilesearchresult.html",
+        params=params,
+        taskid=res["id"],
+        dblist=wh.get_dblist(),
+    )
+
 
 @bp.route("/go/autocomplete/<int:taxid>")
 def go_autocomplete(taxid):
     pattern = request.args.get("term")
     return jsonify(go.search_term(pattern, taxid))
 
+
 @bp.route("/gosearch")
 def go_search():
-    return render_template('gosearch.html', dblist=wh.get_dblist())
+    return render_template("gosearch.html", dblist=wh.get_dblist())
 
-@bp.route("/gosearch/result", methods=['POST'])
+
+@bp.route("/gosearch/result", methods=["POST"])
 def go_search_run():
     params = {}
-    for key in ['database', 'release', 'taxid', 'goid', 'species_name', 'goname']:
+    for key in ["database", "release", "taxid", "goid", "species_name", "goname"]:
         params[key] = request.form[key]
-    job_params = {k: v for k, v in params.items() if k not in ['species_name', 'goname']}
-    res = submit_task(config['worker_pool']['host'], 'go_search', job_params)
-    return render_template('gosearchresult.html', params=params, taskid=res['id'], dblist=wh.get_dblist())
+    job_params = {
+        k: v for k, v in params.items() if k not in ["species_name", "goname"]
+    }
+    res = submit_task(config["worker_pool"]["host"], "go_search", job_params)
+    return render_template(
+        "gosearchresult.html", params=params, taskid=res["id"], dblist=wh.get_dblist()
+    )
+
 
 @bp.route("/go/match/<database>/<int:release>/<int:taxid>/<goid>")
 def go_match(database, release, taxid, goid):
@@ -213,45 +294,58 @@ def go_match(database, release, taxid, goid):
         abort(404)
     return jsonify(db.search_by_go(goid, taxid, goservice=go))
 
+
 @bp.route("/profilesearch/result/<taskid>")
 @bp.route("/gosearch/result/<taskid>")
 def profile_search_res(taskid):
-    res = check_task(config['worker_pool']['host'], taskid)
+    res = check_task(config["worker_pool"]["host"], taskid)
     return jsonify(res)
+
 
 @bp.route("/downloads")
 def data():
-    return render_template('data.html', dblist=wh.get_dblist(), base_url=config['warehouse']['data_server_url'])
+    return render_template(
+        "data.html",
+        dblist=wh.get_dblist(),
+        base_url=config["warehouse"]["data_server_url"],
+    )
+
 
 @bp.route("/data/<database>/<int:release>")
 def db_data(database, release):
     db = wh.get_db(database, release)
     if not db:
         abort(404)
-    data = {
-        "species": db.get_species_list()
-    }
+    data = {"species": db.get_species_list()}
     return jsonify(data)
+
 
 @bp.route("/database/<database>/<int:release>")
 def db_details(database, release):
     db = wh.get_db(database, release)
     if not db:
         abort(404)
-    return render_template("database.html",
-                            db=db.get_info(),
-                            stats=db.get_stats(),
-                            species=db.get_species_list(),
-                            dblist=wh.get_dblist()
-                        )
+    return render_template(
+        "database.html",
+        db=db.get_info(),
+        stats=db.get_stats(),
+        species=db.get_species_list(),
+        dblist=wh.get_dblist(),
+    )
 
+
+# Works
 @bp.route("/databases")
 def db_list():
-    return render_template('dblist.html', dblist=wh.get_dblist(), dbstats=wh.gather_stats())
+    return render_template(
+        "dblist.html", dblist=wh.get_dblist(), dbstats=wh.gather_stats()
+    )
+
 
 @bp.route("/api")
 def page_api():
     return redirect("https://lbgi.fr/api/index.rvt?api=orthoinspector")
+
 
 @bp.route("/<page>")
 def default(page):
@@ -260,6 +354,7 @@ def default(page):
     except jinja2.exceptions.TemplateNotFound:
         abort(404)
 
-app.register_blueprint(bp, url_prefix=config['prefix'])
-app.register_blueprint(bpapi, url_prefix=config['prefix']+'/api')
-app.register_blueprint(bpsecret, url_prefix=config['prefix'])
+
+app.register_blueprint(bp, url_prefix=config["prefix"])
+app.register_blueprint(bpapi, url_prefix=config["prefix"] + "/api")
+app.register_blueprint(bpsecret, url_prefix=config["prefix"])
