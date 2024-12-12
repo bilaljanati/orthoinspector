99822dddc1718ae17af8f7922d58c3cd3e261b07
janati <janati@lbgi.fr>
2024-12-12 15:49:57 +0100

    started debugging the library

--- library.py
+++ library.py
@@ -13,15 +13,15 @@ def orthoDB():
 	data = json.loads(response.read())
 	return data
 
-class database(object):
+class Database:
 	def __init__(self, name, release):
 		self.database = name
 		self.release = release
 		self.url = f"http://localhost:5000/oi/api/{self.database}/{self.release}"
-		species_list = None
-		newick = None
-		clades = None
-	def get_species_liste(self):
+		self.species_list = None
+		self.newick = None
+		self.clades = None
+	def get_species_list(self):
 		if self.species_list == None:
 			query = self.url + "/species"
 			response = open.urlopen(query)
@@ -29,7 +29,7 @@ class database(object):
 		return self.species_list
 	def get_tree_newick(self):
 		if self.newick == None:
-                        query = self.url + "/clades/Newick"
+                        query = self.url + "/clades/newick"
                         response = open.urlopen(query)
                         self.newick = json.loads(response.read())
 		return self.newick
@@ -40,12 +40,12 @@ class database(object):
                         self.clades = json.loads(response.read())
 		return self.clades
 
-class species(object): #passer par un objet bdd
+class Species: #passer par un objet bdd
 	def __init__(self, db, taxid):
 		self.taxid = taxid
-		self.database = self.db.database
-		self.release = self.db.release
-		self.url = self.db.url
+		self.database = db.database
+		self.release = db.release
+		self.url = db.url
 		self.proteins = None
 		self.profiles = None
 	def get_all_protein(self):
@@ -56,32 +56,32 @@ class species(object): #passer par un objet bdd
 		return self.proteins
 	def get_proteins_profiles(self):
 		if self.profiles == None:
-                	query = url + f"/species/{self.taxid}/profiles"
+                	query = self.url + f"/species/{self.taxid}/profiles"
                 	response = open.urlopen(query)
                 	self.profiles = json.loads(response.read())
 		return self.profiles
 	def orthologs_with(self, taxid2):
-                query = url + f"/species/{self.taxid}/orthologs/{taxid2}"
+                query = self.url + f"/species/{self.taxid}/orthologs/{taxid2}"
                 response = open.urlopen(query)
                 orthologs = json.loads(response.read())
                 return orthologs
 
 
-class protein(object):
+class Protein:
 	def __init__(self, access, sp):
 		self.access = access
-		self.taxid = self.sp.taxid
-		self.database = self.sp.database
-		self.url = self.sp.url
-		self.release = self.sp.release
+		self.taxid = sp.taxid
+		self.database = sp.database
+		self.url = sp.url
+		self.release = sp.release
 		self.info = None
 		self.orthologs = None
 		self.profile = None
 	def info(self):
 		if self.info == None:
 			query = url + f"/protein/{self.access}"
-                	response = open.urlopen(query)
-                	self.info = json.loads(response.read())
+			response = open.urlopen(query)
+			self.info = json.loads(response.read())
 		return sel.info
 	def orthologs(self, taxid2 = None):
 		if self.taxid2 != None:
